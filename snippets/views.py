a2a698140a87d27319cf90397f93639eed53d00a
Klim <subbotin.klim@gmail.com>
2023-03-09 21:41:59 +0100

    add views to user data

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import User
 from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import status, mixins, generics
 from rest_framework.views import APIView
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 
 # Create your views here.
@@ -15,6 +16,9 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -22,3 +26,13 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
