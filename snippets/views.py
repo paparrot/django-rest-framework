f3e4dc00001bd97d644c2dc9439dd8f57850789c
Klim <subbotin.klim@gmail.com>
2023-03-09 22:00:40 +0100

    add permissions to SnippetDetail view

--- snippets/views.py
+++ snippets/views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
-from django.http import Http404
-from rest_framework.response import Response
-from rest_framework import status, mixins, generics
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from .permissions import IsOwnerOrReadOnly
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
@@ -26,6 +24,11 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
 
 
 class UserList(generics.ListAPIView):
