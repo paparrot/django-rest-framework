32355a711abd7ffebe3422cbe347cf81996c5f78
Klim <subbotin.klim@gmail.com>
2023-03-10 22:22:38 +0100

    add main route and highlight route

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 from .permissions import IsOwnerOrReadOnly
 
 from snippets.models import Snippet
@@ -7,6 +11,23 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 
 
 # Create your views here.
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
 class SnippetList(generics.ListCreateAPIView):
     """
     List all code snippets, or create a new snippet.
