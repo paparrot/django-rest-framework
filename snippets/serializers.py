1881ddaebc22e3ef521f493fd7b06aa96832e411
Klim <subbotin.klim@gmail.com>
2023-03-10 22:48:00 +0100

    add hyperlinks to snippets and highlight

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,18 +1,19 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'owner', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'owner', 'title', 'highlight', 'code', 'linenos', 'language', 'style']
 
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet', read_only=True)
 
     class Meta:
         model = User
